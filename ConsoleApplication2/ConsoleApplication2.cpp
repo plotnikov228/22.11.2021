840a4d9fd74822ba00b909665657b581ae2cb717
Роман Плотников <plotnikovv@VSPC34.RU>
2021-11-24 19:06:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int k, n, p, m;
+    cout << "n=";
+    cin >> n;
+    p = 10;
+    for (k = 1; k <= n; k++)
+    {
+        m = k * k;
+        if (p < k) p *= 10;
+        if (m % p == k)
+            cout << k << " " << m << endl;
+    }
+    system("pause");
+}
+
+// 15.    найти номер первого элемента, который совпадает с квад-ратом последующего.
