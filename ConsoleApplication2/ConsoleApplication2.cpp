e8aef4f10982152fb39c0caff6cb82fd3b6af202
plotnikov228 <92577632+plotnikov228@users.noreply.github.com>
2021-11-29 10:18:36 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,15 +2,25 @@
 #include<stdlib.h>
 #include<math.h>
 #include<iostream>
-
 using namespace std;
+
+void findEvensInArray(int* array, int length);
+void fillArray(int* array, int length);
+void clear();
+
 int main()
 {
+    
+    fillArray(array, 22);
+    findEvensInArray(array, 22);
+
+    return 0;
+    
     int k, n, p, m;
-    cout << "n=";
+    cout << "n= ";
     cin >> n;
     p = 10;
-    for (k = 1; k <= n; k++)
+    for (int k = 1; k <= n; k++)
     {
         m = k * k;
         if (p < k) p *= 10;
@@ -18,6 +28,29 @@ int main()
             cout << k << " " << m << endl;
     }
     system("pause");
+}
+
+void fillArray(int* array, int length, int m)
+{
+     
+     for (int k = 1; k <= n; k++)
+     {
+        m = k * k;
+     }
+}
+
+void findEvensInArray(int* array, int length,)
+{
+     cout << "n= ";
+     cin >> n;
+     p = 10;
+    if (p < k) p *= 10;
+    {
+    if (m % p == k)
+        {    
+            cout << k << " " << m << endl;
+        }
+    }
 }
 
 // 15.    найти номер первого элемента, который совпадает с квад-ратом последующего.
